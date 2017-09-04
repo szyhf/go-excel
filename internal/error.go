c23c5bb367d63160a7ce55c6b7c110583b0dab39
Back Yu <yhfszb@gmail.com>
2017-09-04 09:33:09 +0800

    internal: 补充errors

---
+++ internal/error.go
@@ -0,0 +1,10 @@
+package internal
+
+import "errors"
+
+var (
+	ErrConnectNotOpened      = errors.New("connect should opened.")
+	ErrWorkbookNotExist      = errors.New("xl/workbook.xml not exist.")
+	ErrWorksheetsNotExist    = errors.New("xl/worksheets/*.xml not exist.")
+	ErrSharedStringsNotExist = errors.New("xl/sharedStrings.xml not exist.")
+)
