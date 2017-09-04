5454a2ba2f961e34c28de91dee293749e8e134cb
Back Yu <yhfszb@gmail.com>
2017-09-04 09:32:55 +0800

    internal: 整理读取文件

--- internal/connect.go
+++ internal/connect.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"archive/zip"
+	"errors"
+	"strings"
 
 	"github.com/szyhf/go-excel"
 )
@@ -12,19 +14,22 @@ const (
 	// 找个各个sheet的名字的地方
 	WORK_BOOK = "xl/workbook.xml"
 	// 各个工作表的数据
-	WORK_SHEETS = "xl/worksheets/sheet*.xml"
+	WORK_SHEETS_PREFIX = "xl/worksheets/sheet"
 )
 
 type Connect struct {
 	FilePath string
 
+	// list of sorted sheet name
+	sheets []string
+
 	// xl/sharedStrings.xml
 	sharedStringsFile *zip.File
 	// xl/workbook.xml
-	workBookFile *zip.File
+	workbookFile *zip.File
 	// "xl/worksheets/sheet*.xml" map[sheet*]*zip.File
-	sheetFileMap map[string]*zip.File
-	zipReader    *zip.ReadCloser
+	worksheetFileMap map[string]*zip.File
+	zipReader        *zip.ReadCloser
 }
 
 func NewConnect() excel.Connecter {
@@ -36,6 +41,13 @@ func (this *Connect) Open(filePath string) error {
 	var err error
 	this.zipReader, err = zip.OpenReader(filePath)
 	if err != nil {
+		return err
+	}
+	// prepare for files
+	err = this.init()
+	if err != nil {
+		this.zipReader.Close()
+		this.zipReader = nil
 		return err
 	}
 	return nil
@@ -56,6 +68,7 @@ func (this *Connect) NewReader(sheet string) (excel.Reader, error) {
 	if this.zipReader == nil {
 		return nil, ErrConnectNotOpened
 	}
+
 	return nil, nil
 }
 
@@ -67,6 +80,34 @@ func (this *Connect) MustReader(sheet string) excel.Reader {
 	return rd
 }
 
-func (this *Connect) init() {
+func (this *Connect) init() error {
 	// Find file of "workbook.xml", "sharedStrings.xml" and files in worksheets
+	this.worksheetFileMap = make(map[string]*zip.File)
+	for _, f := range this.zipReader.File {
+		switch f.Name {
+		case SHARED_STRING:
+			this.sharedStringsFile = f
+		case WORK_BOOK:
+			this.workbookFile = f
+		default:
+			if strings.HasPrefix(f.Name, WORK_SHEETS_PREFIX) {
+				// Trim left of prefix
+				// Trim right of ".xml" as len = 4
+				worksheetName := f.Name[len(WORK_SHEETS_PREFIX) : len(f.Name)-4]
+				this.worksheetFileMap[worksheetName] = f
+			}
+		}
+	}
+	if this.workbookFile == nil {
+		return ErrWorkbookNotExist
+	}
+	if this.sharedStringsFile == nil {
+		return ErrSharedStringsNotExist
+	}
+	if this.worksheetFileMap == nil || len(this.worksheetFileMap) == 0 {
+		return ErrSharedStringsNotExist
+	}
+	return nil
+}
+
 }
