50e9fc7fb33ca68ec21e2f7f98c044becce80a28
Back Yu <yhfszb@gmail.com>
2017-09-04 08:29:14 +0800

    excel: 更新接口

--- type.go
+++ type.go
@@ -1,8 +1,14 @@
 package excel
 
 type Connecter interface {
-	Open(file string)
-	NewReader(sheet string) Reader
+	// Open a file of excel
+	Open(filePath string) error
+	// Close file reader
+	Close() error
+	// Generate an new reader of a sheet
+	NewReader(sheet string) (Reader, error)
+	// Panic if can not generate a new reader
+	MustReader(sheet string) Reader
 }
 
 type Reader interface {
