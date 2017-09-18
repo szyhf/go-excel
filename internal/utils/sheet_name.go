766fa9119ed3030f0025d8364bbe23a5ebe762e6
Back Yu <yhfszb@gmail.com>
2017-09-18 15:05:20 +0800

    增加从slice获取sheet name的逻辑

--- internal/utils/sheet_name.go
+++ internal/utils/sheet_name.go
@@ -15,6 +15,10 @@ func ParseSheetName(i interface{}) string {
 	default:
 		val := reflect.ValueOf(i)
 		typ := reflect.Indirect(val).Type()
+		switch typ.Kind() {
+		case reflect.Slice, reflect.Ptr:
+			typ = typ.Elem()
+		}
 		return typ.Name()
 	}
 }
