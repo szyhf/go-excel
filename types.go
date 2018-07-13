5ddae9503542f001f794fad72e2cf1faa5c7bea7
Back Yu <yhfszb@gmail.com>
2018-07-13 19:06:30 +0800

    fix comments.

--- types.go
+++ types.go
@@ -3,6 +3,7 @@ package excel
 // Config of connecter
 type Config struct {
 	// sheet: if sheet is string, will use sheet as sheet name.
+	//        if sheet is int, will i'th sheet in the workbook, be careful the hidden sheet is counted. i ∈ [1,+inf]
 	//        if sheet is a object implements `GetXLSXSheetName()string`, the return value will be used.
 	//        otherwise, will use sheet as struct and reflect for it's name.
 	// 		  if sheet is a slice, the type of element will be used to infer like before.
@@ -47,8 +48,8 @@ type Connecter interface {
 	// sheetNamer: if sheetNamer is string, will use sheet as sheet name.
 	//             if sheetNamer is int, will i'th sheet in the workbook, be careful the hidden sheet is counted. i ∈ [1,+inf]
 	//             if sheetNamer is a object implements `GetXLSXSheetName()string`, the return value will be used.
+	// 	           if sheetNamer is a slice, the type of element will be used to infer like before.
 	//             otherwise, will use sheetNamer as struct and reflect for it's name.
-	// 			   if sheetNamer is a slice, the type of element will be used to infer like before.
 	NewReader(sheetNamer interface{}) (Reader, error)
 	// Generate an new reader of a sheet
 	// sheetNamer: if sheetNamer is string, will use sheet as sheet name.
